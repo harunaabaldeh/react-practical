1a3ba8b0c0ab2f1f296783dc0cd5e027dd3fd86a
Haruna <mahmooddrammeh@haruna.local>
2021-07-29 09:51:17 +0000

    added some components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-    <p>Hello World</p>
-    {/* I need to add some my code  */}
+    <Info />
+    <AddItem />
+    <AvailablePeople />
+    </div>
+  );
+}
+
+function Info(){
+  const title = 'This is my title';
+
+  return(
+    <div>
+      {title}
+      <h1>Inventory Management </h1>
+      <p>Manage your stuffs</p>
+      <Info />
+    </div>
+  );
+}
+
+function AddItem(){
+  return(
+    <form>
+      <label for='text-form'>Type something</label>
+      <input type="text" id="text-form" />
+    </form>
+  );
+}
+
+function AvailablePeople(){
+  return(
+    <div>
+     <h1>This is the place to find all the doctors and their special characters</h1>
     </div>
   );
 }
