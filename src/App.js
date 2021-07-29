3b171246aee8235f1fc0657d5ebb9f923a82b495
harunaabaldeh <harunaabadeh@gmail.com>
2021-07-29 09:52:36 +0100

    added info component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <p>Hello World</p>
-    {/* I need to add some my code  */}
+      <p>Hello World</p>
+      <Info />
+    </div>
+  );
+}
+
+function Info() {
+  return (
+    <div>
+      <p>Anonymous Counselling Web </p>
     </div>
   );
 }
